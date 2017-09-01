a9d43bef51cf67e5687b2f1f94248e22af27f540
Padalingam <padalingam@nalashaa.com>
2017-09-01 15:37:04 +0530

    Inial Commit

---
+++ CameraAppTwo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace CameraAppTwo
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new CameraAppTwoPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
